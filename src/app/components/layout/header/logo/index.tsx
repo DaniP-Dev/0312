6333cb1a0d3812c3efe1a804c5951c8e3cb8d120
DaniP-Dev <dperezg23@outlook.com>
2026-01-24 08:06:37 -0500

    subir a github

---
+++ src/app/components/layout/header/logo/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Logo: React.FC = () => {
+
+  return (
+    <Link href="/">
+      LOGO
+    </Link>
+  );
+};
+
+export default Logo;
