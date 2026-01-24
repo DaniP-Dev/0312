cdf3ae973d777c83578f614db82d5f6eeefefff1
DaniP-Dev <dperezg23@outlook.com>
2026-01-24 08:41:12 -0500

    imagenes

--- src/app/components/documentation/Introduction.tsx
+++ src/app/components/documentation/Introduction.tsx
@@ -3,12 +3,13 @@
 
 "use client"
 import Image from "next/image"
-import nextImg from "/public/images/documentation/Categories=Nextjs.svg"
-import reactImg from "/public/images/documentation/Categories=React.svg"
-import tailwindImg from "/public/images/documentation/Categories=Tailwind.svg"
-import nextauthImg from "/public/images/documentation/nextauth.png"
-import typescriptImg from "/public/images/documentation/Categories=Typescript.svg"
-import axiosImg from "/public/images/documentation/axios.svg"
+// Use public path strings for images (Next.js best practice)
+const nextImg = "/images/documentation/Categories=Nextjs.svg";
+const reactImg = "/images/documentation/Categories=React.svg";
+const tailwindImg = "/images/documentation/Categories=Tailwind.svg";
+const nextauthImg = "/images/documentation/nextauth.png";
+const typescriptImg = "/images/documentation/Categories=Typescript.svg";
+const axiosImg = "/images/documentation/axios.svg";
 import { Icon } from "@iconify/react/dist/iconify.js"
 import { useState } from "react"
 import { DocNavigation } from "./DocNavigation"
